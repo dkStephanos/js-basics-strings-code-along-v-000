be7e26c585dd775c03b7a3bc69a9d0b654195e3a
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-03 19:15:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const currentUser = "Abel Tesfaye";
 
 const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
 
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.capitalize + "!";
 
 const shortGreeting = "Welcome, " + currentUser + "!";
